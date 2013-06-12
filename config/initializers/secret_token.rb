c61ff6d90ef9d31cb28b4126540b7891105a9d61
slimBN <elliji@yopmail.com>
2013-06-12 18:44:46 +0100

    home ready

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Punaise::Application.config.secret_token = 'b3a1dd9be423eb19556d96dd6706b7c3725e58afb7b7702c308e20d873d1772e96a0739b14f5506426eb051f20271235ae42f333cfe1f5e253b1699de4d019fb'
