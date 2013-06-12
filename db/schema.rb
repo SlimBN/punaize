9756ddac98b9ba57b9dd7fafd08ff729d176e852
slimBN <elliji@yopmail.com>
2013-06-12 21:29:05 +0100

    just rake db:migrate wbarra

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,27 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130612182439) do
+ActiveRecord::Schema.define(:version => 20130612195143) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.string   "icon"
+    t.string   "slug"
+    t.text     "description"
+    t.boolean  "activated"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "mains", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "states", :force => true do |t|
+    t.string   "name"
+    t.string   "slug"
+    t.integer  "country_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
